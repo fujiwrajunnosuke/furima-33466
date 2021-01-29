8b5e7e28c81d66aeb79e09ad7d034e7b741b7a58
fujiwrajunnosuke <ponjun0529@gmail.com>
2021-01-30 04:02:03 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_day
+  belongs_to :delivery_prefecture
+  belongs_to :state
+  belongs_to :delivery_price
   belongs_to :user
   has_one_attached :image
+
+
 end
