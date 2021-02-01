7ea498eb994e95fc4389a44d98ae7a0cbc8e4539
fujiwrajunnosuke <ponjun0529@gmail.com>
2021-02-01 18:30:57 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,6 +72,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
+
+      it "priceが300円未満では登録できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+
+      it "priceが10,000,000を超えると登録できない" do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
     end
   end
 end
