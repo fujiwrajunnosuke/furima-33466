1e2af185c475e5bb847dfd396dde2c5e0f5833cd
fujiwrajunnosuke <ponjun0529@gmail.com>
2021-02-05 14:59:32 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :introduction, :category_id, :state_id, :delivery_price_id, :delivery_prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)  
